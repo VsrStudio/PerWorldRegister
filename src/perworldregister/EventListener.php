251c2bfe40be3f6a81ffc97ee81c509ca3cdd798
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-12-04 07:41:34 +0700

    Update Fix EventListener.php

--- src/perworldregister/EventListener.php
+++ src/perworldregister/EventListener.php
@@ -4,6 +4,7 @@ namespace perworldregister;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\player\Player;
 
 class EventListener implements Listener {
@@ -23,6 +24,23 @@ class EventListener implements Listener {
                 $this->plugin->showRegisterForm($player);
             } elseif (!$this->plugin->isPlayerLoggedIn($player->getName())) {
                 $this->plugin->showLoginForm($player);
+            }
+        }
+    }
+
+    public function onTeleport(EntityTeleportEvent $event): void {
+        $entity = $event->getEntity();
+        
+        if ($entity instanceof Player) {
+            $fromWorld = $event->getFrom()->getWorld()->getFolderName();
+            $toWorld = $event->getTo()->getWorld()->getFolderName();
+
+            if ($fromWorld !== $toWorld && $this->plugin->isWorldProtected($toWorld)) {
+                if (!$this->plugin->isPlayerRegistered($entity->getName())) {
+                    $this->plugin->showRegisterForm($entity);
+                } elseif (!$this->plugin->isPlayerLoggedIn($entity->getName())) {
+                    $this->plugin->showLoginForm($entity);
+                }
             }
         }
     }
